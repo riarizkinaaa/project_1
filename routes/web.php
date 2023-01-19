302aad897d8df1a07e8c30b25f9d21b31867449a
riarizkinaaa <riarizkina20@gmail.com>
2023-01-19 10:02:59 +0800

    template_layout

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth.login');
 });
+Route::get('/pendata', function () {
+    return view('pendata.pendata');
+});
